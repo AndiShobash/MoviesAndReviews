1d1994d78a34c47522877299b18ab655459ff53d
AndiShobash <andi500@gmail.com>
2023-06-11 16:42:22 +0300

    Creating MovieController for Movies

--- src/main/java/dev/andi/movies/MovieController.java
+++ src/main/java/dev/andi/movies/MovieController.java
@@ -1,9 +1,28 @@
 package dev.andi.movies;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    private final MovieService movieService;
+
+    //private final MovieRepository movieRepository;
+
+    public MovieController(MovieService movieService, MovieRepository movieRepository) {
+        this.movieService = movieService;
+        //this.movieRepository = movieRepository;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.AllMovies(), HttpStatus.OK);
+        //return new ResponseEntity<List<Movie>>(movieRepository.findAll(), HttpStatus.OK);
+    }
 }

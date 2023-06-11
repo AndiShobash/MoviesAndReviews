58bd0deb9e7b2eb83769361b27afef9f0c2ccfa2
AndiShobash <andi500@gmail.com>
2023-06-11 19:44:26 +0300

    Added more GetRequest and PostRequest

--- src/main/java/dev/andi/movies/MovieController.java
+++ src/main/java/dev/andi/movies/MovieController.java
@@ -1,12 +1,15 @@
 package dev.andi.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -24,5 +27,10 @@ public class MovieController {
     public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.AllMovies(), HttpStatus.OK);
         //return new ResponseEntity<List<Movie>>(movieRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String id){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id),HttpStatus.OK);
     }
 }

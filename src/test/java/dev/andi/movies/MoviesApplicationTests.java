22456c27178b89291e1e631e8b1f644a1f1f46eb
AndiShobash <andi500@gmail.com>
2023-06-11 16:14:49 +0300

    Initialize Project

---
+++ src/test/java/dev/andi/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andi.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
